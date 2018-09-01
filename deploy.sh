a7702f56dc80f1fc9603f7c67c1bf991bfc9f9bd
kaleb borda fonseca <kalebbf.dev@gmail.com>
2018-09-01 17:47:58 -0300

    ajuste

--- deploy.sh
+++ deploy.sh
@@ -27,7 +27,7 @@ if [[ "${reslog}" != "" ]] ; then
 	ln -s  $envFile $pathFile/$nameProject/.env
 
 	echo "Gera o link simbolico no novo projeto"
-	ln -s /var/storage/ $pathFile/$nameProject/storage/ 
+	ln -s    /var/storage/ $pathFile/$nameProject/storage/
 
 	echo "migrando as atualizações de banco"
 #	php $pathFile/novo-sistema/artisan migrate
